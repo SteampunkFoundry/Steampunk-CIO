6710cc9dc93617cc401f1811c49e3c8407b91613
Michael Kim <michael.kim@steampunk.com>
2022-04-28 11:17:47 -0400

    Update copying function to target only subdirs.

--- project-repository-automation/project-repository-automation.ps1
+++ project-repository-automation/project-repository-automation.ps1
@@ -13,10 +13,14 @@ Write-Host "Creating the $($Title) document library"
 New-PnPList -Title $Title -Template DocumentLibrary
 
 #Copy the folder structure to the new document library
-$Item = Get-PnPListItem -List "Shared Documents" -Query "<View><Query><Where><Eq><FieldRef Name='Title'/><Value Type='Text'>testProject</Value></Eq></Where></Query></View>"
+$Items = Get-PnPListItem -List "Shared Documents"
 $TargetUrl = "/sites/Michael-Kim-Test-Site/$($Name) $($ChargeCode)"
-Write-Host "Copying folder: $($Item.FieldValues.FileLeafRef)"
-Copy-PnPFile -SourceUrl "$($Item.FieldValues.FileRef)" -TargetUrl $TargetUrl -Force
+foreach($Item in $Items){
+    if (($Item.fieldValues.FileRef -match '/sites/Michael-Kim-Test-Site/Shared Documents/testProject/') -and ($Item.fieldValues.FileRef -notmatch '/sites/Michael-Kim-Test-Site/Shared Documents/testProject/.*/')){
+    Write-Host "Copying folder: $($Item.FieldValues.FileLeafRef)"
+    Copy-PnPFile -SourceUrl "$($Item.FieldValues.FileRef)" -TargetUrl $TargetUrl -Force
+    }
+}
 
 #Add List
 Write-Host "Creating List..."
